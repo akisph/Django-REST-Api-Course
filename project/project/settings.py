5821b30f74256df8ce3791a8737f4ecf007bc434
Sotirios Filippou <s.filippou@dataverse.gr>
2021-03-05 11:42:48 +0200

    5/3 Django REST API Completed

--- project/project/settings.py
+++ project/project/settings.py
@@ -135,9 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
-# To customize user
 
 AUTH_USER_MODEL = "app.User"
